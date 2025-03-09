c564b507662f15a2b61e66034d82f266c023634f
KavyadharshiniM06 <kavyadharshinim2@gmail.com>
2025-03-09 13:09:45 +0530

    Add files via upload

---
+++ movielens/movielens/tables.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Movie_database;
+USE Movie_database;
+
+
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
